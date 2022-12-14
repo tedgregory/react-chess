7804c85ad21fd0966a54fd623753bae5de851157
tedgregory <mail@tedgregory.ru>
2022-12-14 19:55:50 +0300

    feat: set up prettier eslint and plugins

---
+++ src/components/BoardComponent/BoardComponent.tsx
@@ -0,0 +1,26 @@
+import CellComponent from '../CellComponent/CellComponent';
+
+export default function BoardComponent() {
+  return (
+    <div className="board">
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+      <CellComponent />
+    </div>
+  );
+}
---
+++ src/components/CellComponent/CellComponent.tsx
@@ -0,0 +1,4 @@
+import pawn from '../../assets/black-pawn.svg';
+export default function CellComponent() {
+  return <div className="cell"></div>;
+}
